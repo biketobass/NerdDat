7d5112c23fdb046a805e71b5c25f4bbc36ba59a9
Bike to Bass <78823174+biketobass@users.noreply.github.com>
2023-03-13 09:35:20 -0400

    Changed logging level from debug to warning. Also set DEBUG in settings to False. It wasn't working as an env var on Railway

--- fitnessapp/settings.py
+++ fitnessapp/settings.py
@@ -21,7 +21,8 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 SECRET_KEY = env('DJANGO_SECRET_KEY')
-DEBUG = env('DJANGO_DEBUG')
+#DEBUG = env('DJANGO_DEBUG')
+DEBUG = False
 ALLOWED_HOSTS = [env('DJANGO_ALLOWED_HOSTS')]
 
 # Application definition
